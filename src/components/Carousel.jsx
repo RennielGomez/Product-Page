458aa0f700b654cb7d9cc0d49eb61328ba717214
RennielGomez <gomezrenniel9@gmail.com>
2023-02-14 18:22:49 +0800

    fixed wrong images sizes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function Carousel() {
 
   const [imgSource, setImgSource] = useState("/images/image-product-1.jpg");
 
   return (
-    <div className='grid grid-cols-4 grid-rows-5 justify-items-stretch gap-4 md:mx-24 lg:mx-auto'>
-      <img className='col-span-4 row-span-4 rounded-xl mb-4 ' src={imgSource} alt="product image" />
-      <img className='col-span-1 rounded-xl md:row-span-1' onClick={()=>{setImgSource("/images/image-product-1.jpg")}} src="/images/image-product-1-thumbnail.jpg" alt="thumnail image 1" />
-      <img className='col-span-1 rounded-xl md:row-span-1' onClick={()=>{setImgSource("/images/image-product-2.jpg")}} src="/images/image-product-2-thumbnail.jpg" alt="thumnail image 2" />
-      <img className='col-span-1 rounded-xl md:row-span-1' onClick={()=>{setImgSource("/images/image-product-3.jpg")}} src="/images/image-product-3-thumbnail.jpg" alt="thumnail image 3" />
-      <img className='col-span-1 rounded-xl md:row-span-1' onClick={()=>{setImgSource("/images/image-product-4.jpg")}} src="/images/image-product-4-thumbnail.jpg" alt="thumnail image 4" />
+    <div className='flex flex-col justify-items-stretch gap-4 md:mx-24 lg:mx-auto'>
+      <img className='rounded-xl mb-4 ' src={imgSource} alt="product image" />
+      <div className='flex'>
+        <img className='img-fluid' onClick={() => { setImgSource("/images/image-product-1.jpg") }} src="/images/image-product-1-thumbnail.jpg" alt="thumnail image 1" />
+        <img className='img-fluid' onClick={() => { setImgSource("/images/image-product-2.jpg") }} src="/images/image-product-2-thumbnail.jpg" alt="thumnail image 2" />
+        <img className='img-fluid' onClick={() => { setImgSource("/images/image-product-3.jpg") }} src="/images/image-product-3-thumbnail.jpg" alt="thumnail image 3" />
+        <img className='img-fluid' onClick={() => { setImgSource("/images/image-product-4.jpg") }} src="/images/image-product-4-thumbnail.jpg" alt="thumnail image 4" />
+      </div>
     </div>
   )
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,11 +3,15 @@ import Carousel from "./Carousel"
 import ProductDetails from "./ProductDetails"
 
 function Container() {
-  
+
   return (
-    <div className='flex items-center mx-auto gap-8 mt-16 lg:w-3/4 sm:width-auto lg:flex-row sm:flex-col'>
-      <Carousel />
-      <ProductDetails/>
+    <div className='grid grid-cols-5 items-center mx-auto gap-8 mt-16 lg:w-3/4 sm:width-auto lg:flex-row sm:flex-col'>
+      <div className='col-span-3'>
+        <Carousel />
+      </div>
+      <div className='col-span-2'>
+        <ProductDetails />
+      </div>
     </div>
   )
 }
